0d0c7437c15ba1e429a68e86717c255d4aa17b8d
Joshua <joshua@lulububu.de>
2025-04-11 22:02:43 +0200

    Trivial improvements

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,25 +1,25 @@
 describe('Example spec', () => {
-  beforeEach(() => {
-    cy.visit('/example/');
-  });
+    beforeEach(() => {
+        cy.visit('/example/');
+    });
 
-  it('should render intro-container', () => {
-    cy.get('#intro-container')
-      .get('h1')
-      .contains('Intro')
-  })
+    it('should render intro-container', () => {
+        cy.get('#intro-container')
+          .get('h1')
+          .contains('Intro');
+    });
 
-  it('should transform value on scroll', () => {
-    cy.get('#intro-container')
-      .get('h1')
-      .should('have.attr', 'style')
-      .should('contain', 'transform: scale(1)')
+    it('should transform value on scroll', () => {
+        cy.get('#intro-container')
+          .get('h1')
+          .should('have.attr', 'style')
+          .should('contain', 'transform: scale(1)');
 
-    cy.scrollTo('0', '300')
+        cy.scrollTo('0', '300');
 
-    cy.get('#intro-container')
-      .get('h1')
-      .should('have.attr', 'style')
-      .should('contain', 'transform: scale(20)')
-  })
-})
+        cy.get('#intro-container')
+          .get('h1')
+          .should('have.attr', 'style')
+          .should('contain', 'transform: scale(20)');
+    });
+});
